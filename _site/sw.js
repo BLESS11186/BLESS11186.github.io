93b89a72695fc6de28589e5af8e7a8a420351756
MacBook <jinsook3248@naver.com>
2021-08-27 03:15:47 +0900

    Fix build fail

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20210827.0253';
+const cacheName = 'chirpy-20210827.0301';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
